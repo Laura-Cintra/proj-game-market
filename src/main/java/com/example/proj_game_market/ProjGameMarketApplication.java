9ef883188a0a9d8dbda460eabb6100f90f2417e9
Laura Cintra <79454879+Laura-Cintra@users.noreply.github.com>
2025-04-24 10:32:56 -0300

    first commit

---
+++ src/main/java/com/example/proj_game_market/ProjGameMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.proj_game_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjGameMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjGameMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proj_game_market/ProjGameMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proj_game_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjGameMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
